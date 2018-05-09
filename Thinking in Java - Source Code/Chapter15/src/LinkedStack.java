e42cb088c7c697c69667c2a215f2abe1d8bbe2cd
mio <461152465@qq.com>
2018-05-09 22:53:44 +0800

    generic Method

--- Thinking in Java - Source Code/Chapter15/src/LinkedStack.java
+++ Thinking in Java - Source Code/Chapter15/src/LinkedStack.java
@@ -1,3 +1,6 @@
+//堆栈类：LinkedStack类中定义内部节点类，类似于struct
+//LinkedStack对外呈现top（Node类型）变量，而top变量一个存储字符串，另一个存储一个Node类型（表示指向的下一个节点值）
+//LinkedStack提供push和pop操作
 public class LinkedStack<T> {
 	private static class Node<U>{
 		U item;
@@ -15,10 +18,10 @@ public class LinkedStack<T> {
 		}
 	}
 	private Node<T> top = new Node<T>();
-	public void push(T item){
+	public void push(T item){ //入栈
 		top = new Node<T>(item, top);
 	}
-	public T pop(){
+	public T pop(){ //出栈
 		T result = top.item;
 		if(!top.end())
 			top = top.next;
