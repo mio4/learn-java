3324b71bbdd2a49c5ee40aac26ad6186fbce4bf0
mio <461152465@qq.com>
2018-08-26 00:14:31 +0800

    .
    .

---
+++ Java Concurrency in Practice/Chapter 2/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args){
+		int a = 10;
+		System.out.println(a);
+		change(a);
+		System.out.println(a);
+	}
+	public static void change(int a){
+		a = 50;
+	}
+}
