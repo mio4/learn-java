7d35f9698f37091cdb8a528859538ad489431819
mio4 <461152465@qq.com>
2018-11-09 11:32:26 +0800

    .

--- Java Grammar/src/Basic/part13/StringBufferDemo.java
+++ Java Grammar/src/Basic/part13/StringBufferDemo.java
@@ -3,6 +3,13 @@ package Basic.part13;
 public class StringBufferDemo {
 	public static void main(String[] args){
 		StringBuffer sb = new StringBuffer();
+/*		StringBuffer sb1 = sb.append(100);
+		System.out.println(sb == sb1);
+		System.out.println(sb);
+		System.out.println(sb1);*/
 
+		sb.append(100);
+		sb.append("mio");
+		System.out.println(sb.toString());
 	}
 }
