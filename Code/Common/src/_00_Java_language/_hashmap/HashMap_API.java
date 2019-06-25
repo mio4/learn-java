2fb1d0f5aff3ed05c7545a84e002e2255118a624
mio4 <mio_180@163.com>
2019-06-26 00:03:31 +0800

    update

--- Code/Common/src/_00_Java_language/_hashmap/HashMap_API.java
+++ Code/Common/src/_00_Java_language/_hashmap/HashMap_API.java
@@ -1,4 +1,4 @@
-package XiaoZhao.JavaLanguage;
+package _00_Java_language._hashmap;
 
 import java.util.HashMap;
 import java.util.Iterator;
@@ -9,16 +9,24 @@ import java.util.Map;
  */
 public class HashMap_API {
 
-    public static void main(String[] args){
+    private static Map<String,String> map;
 
+    public static void main(String[] args){
+        initialize();
+        addNullKey();
     }
 
-    public static Map<Integer,Integer> getHashMap(){ //PUT
-        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
-        map.put(1,10);
-        map.put(2,20);
-        map.put(3,30);
-        return map;
+    public static void initialize(){
+        map = new HashMap<>();
+        map.put("key1","value1");
+        map.put("key2","value2");
+        map.put("key3","value3");
+        map.put("key4","value4");
+    }
+
+    public static void addNullKey(){
+        map.put(null,"null value");
+        System.out.println(map.get(null));
     }
 
     public static void test_traverse(Map<Integer,Integer> map){ //HashMap的遍历
