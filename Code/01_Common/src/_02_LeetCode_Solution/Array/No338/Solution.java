08964c2f5b40d4dee6bd92bfefd8846d359cf878
zhangjin19 <zhangjin19@baidu.com>
2019-07-02 11:22:31 +0800

    update

---
+++ Code/01_Common/src/_02_LeetCode_Solution/Array/No338/Solution.java
@@ -0,0 +1,15 @@
+package _02_LeetCode_Solution.Array.No338;
+
+/**
+ * 这是一道典型的面试算法题
+ */
+class Solution {
+    public int[] countBits(int num) {
+        
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Code/01_Common/src/readme.md
@@ -0,0 +1,10 @@
+1. 面试中的滑动窗口问题
+
+
+---
+Java
+1. 获取长度
+    数组类型 int[] a.length 属性
+    字符串类型 String s.length() 方法
+    列表类型 ArrayList<T> list.size() 方法
+    
