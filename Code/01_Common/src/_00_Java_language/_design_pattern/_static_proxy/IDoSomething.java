264818fbb949a16651a9e6f83031ebea0456fcc7
zhangjin19 <zhangjin19@baidu.com>
2019-07-07 23:36:02 +0800

    .

---
+++ Code/01_Common/src/_00_Java_language/_design_pattern/_static_proxy/IDoSomething.java
@@ -0,0 +1,4 @@
+package _00_Java_language._design_pattern._static_proxy;
+
+public class IDoSomething {
+}
---
+++ Code/01_Common/src/_00_Java_language/_dynamic_proxy/_jdk/MyInvocationHandler.java
@@ -0,0 +1,4 @@
+package _00_Java_language._dynamic_proxy._jdk;
+
+public class MyInvocationHandler {
+}
