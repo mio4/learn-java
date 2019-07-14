90d27cdc33f58207bbb3e5817cbad4ca25fbec59
mio4 <461152465@qq.com>
2019-07-14 19:35:31 +0800

    update

--- Code/01_Common/src/_00_Java_language/HashMap_API.java
+++ Code/01_Common/src/_00_Java_language/HashMap_API.java
@@ -15,7 +15,6 @@ public class HashMap_API {
         initialize();
         addNullKey();
         test_traverse();
-
 //        testUse();
     }
 
--- Code/01_Common/src/_00_Java_language/_dynamic_proxy/_cglib/Target.java
+++
@@ -1,5 +0,0 @@
-package _00_Java_language._dynamic_proxy._cglib;
-
-public interface Target {
-    void request();
-}
