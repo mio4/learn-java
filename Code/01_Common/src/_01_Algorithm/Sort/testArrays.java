0f50ce703333bc7ec02d9cbede6e44b61135e4c0
mio4 <461152465@qq.com>
2019-07-20 18:15:09 +0800

    update

--- Code/01_Common/src/_01_Algorithm/Sort/testArrays.java
+++ Code/01_Common/src/_01_Algorithm/Sort/testArrays.java
@@ -8,8 +8,15 @@ public class testArrays {
 
     @Test
     public void test(){
-        int[] a = {1,2,3};
-        int[] b = Arrays.copyOf(a,3);
-        System.out.println(Arrays.toString(b));
+//        int[] a = {1,2,3};
+//        int[] b = Arrays.copyOf(a,3);
+//        System.out.println(Arrays.toString(b));
+
+//        int a = 6249;
+//        int b = 419;
+        int a = 383;
+        int b = 186;
+        System.out.println(a % b);
+        System.out.println(a / b);
     }
 }
