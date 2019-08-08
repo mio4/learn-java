156b85fc127d5b26d4af6b3ebbe7c99596377164
zhangjin19 <zhangjin19@baidu.com>
2019-08-08 13:52:38 +0800

    update

---
+++ Code/01_Common/src/_02_LeetCode_Solution/Interview/_494/Solution.java
@@ -0,0 +1,24 @@
+package _02_LeetCode_Solution.Interview._494;
+
+class Solution {
+    private static int res = 0;
+    public static int findTargetSumWays(int[] nums, int S) {
+        helper(nums,0,S);
+        return res;
+    }
+
+    private static void helper(int[] nums,int index,int sum){
+        if(index == 0){
+            if(sum == 0)
+                res++;
+            return;
+        }
+        helper(nums,index+1,sum-nums[index]);
+        helper(nums,index+1,sum+nums[index]);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,1,1};
+        System.out.println(findTargetSumWays(nums,3));
+    }
+}
