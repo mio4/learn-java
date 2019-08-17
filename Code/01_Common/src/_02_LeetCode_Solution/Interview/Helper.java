f8defa2a5fb2cc6e9e24f513f7ca9e906d22fa08
mio4 <461152465@qq.com>
2019-08-17 15:03:57 +0800

    update

--- Code/01_Common/src/_02_LeetCode_Solution/Interview/Helper.java
+++ Code/01_Common/src/_02_LeetCode_Solution/Interview/Helper.java
@@ -23,6 +23,41 @@ public class Helper {
         return t1;
     }
 
-//    private ReentrantLock
+    /**
+     *
+     * @return 1-2-3-2-1
+     */
+    public static ListNode get2(){
+        ListNode t1 = new ListNode(1);
+        ListNode t2 = new ListNode(2);
+        ListNode t3 = new ListNode(3);
+        ListNode t4 = new ListNode(2);
+        ListNode t5 = new ListNode(1);
+
+        t1.next = t2;
+        t2.next = t3;
+        t3.next = t4;
+        t4.next = t5;
+
+        return t1;
+    }
+
+    /**
+     *
+     * @return 1-2-2-1
+     */
+    public static ListNode get3(){
+        ListNode t1 = new ListNode(1);
+        ListNode t2 = new ListNode(2);
+        ListNode t3 = new ListNode(2);
+        ListNode t4 = new ListNode(1);
+
+        t1.next = t2;
+        t2.next = t3;
+        t3.next = t4;
+
+        return t1;
+    }
+
 
 }
